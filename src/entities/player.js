c1d507439da3f8337c558ea6440ca1677492a722
Bisao <godofglobal@gmail.com>
2026-01-23 21:32:58 -0300

    Update player.js

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,36 +1,29 @@
-// src/entities/player.js
-
 export class Player {
     constructor(id, nickname, isLocal = false) {
         this.id = id;
         this.nickname = nickname;
         this.isLocal = isLocal;
         this.pos = { x: 0, y: 0 };
-        this.targetPos = { x: 0, y: 0 }; // Para suavização (interpolação)
-        this.pollen = 0;
+        this.targetPos = { x: 0, y: 0 };
         this.speed = 0.15;
     }
 
     update() {
-        if (this.isLocal) {
-            // Lógica de entrada de teclado será injetada aqui
-        } else {
-            // Suaviza o movimento dos outros jogadores (Interpolação Linear)
-            this.pos.x += (this.targetPos.x - this.pos.x) * 0.1;
-            this.pos.y += (this.targetPos.y - this.pos.y) * 0.1;
+        if (!this.isLocal) {
+            this.pos.x += (this.targetPos.x - this.pos.x) * 0.2;
+            this.pos.y += (this.targetPos.y - this.pos.y) * 0.2;
         }
     }
 
-    draw(ctx, camera) {
-        const screenX = (this.pos.x - camera.x) * 32 + window.innerWidth / 2;
-        const screenY = (this.pos.y - camera.y) * 32 + window.innerHeight / 2;
-
-        ctx.fillStyle = this.isLocal ? "yellow" : "orange";
+    draw(ctx, cam, canvas, tileSize) {
+        const sX = (this.pos.x - cam.x) * tileSize + canvas.width / 2;
+        const sY = (this.pos.y - cam.y) * tileSize + canvas.height / 2;
+        ctx.fillStyle = this.isLocal ? "#f1c40f" : "#e67e22";
         ctx.beginPath();
-        ctx.arc(screenX, screenY, 10, 0, Math.PI * 2);
+        ctx.arc(sX, sY, 12, 0, Math.PI * 2);
         ctx.fill();
-        
         ctx.fillStyle = "white";
-        ctx.fillText(this.nickname, screenX - 20, screenY - 15);
+        ctx.textAlign = "center";
+        ctx.fillText(this.nickname, sX, sY - 20);
     }
 }

6604280451d8d1ace0879c40d10dab1239ec10e7
Bisao <godofglobal@gmail.com>
2026-01-24 06:42:04 -0300

    Update worldState.js

--- src/world/worldState.js
+++ src/world/worldState.js
@@ -1,43 +1,27 @@
 export class WorldState {
     constructor() {
-        this.modifiedTiles = {}; // "x,y": "TIPO"
-        this.growingPlants = {}; // "x,y": timestamp (Host only)
+        this.modifiedTiles = {}; 
+        this.growingPlants = {}; 
     }
-
     setTile(x, y, type) {
         const key = `${x},${y}`;
         if (this.modifiedTiles[key] === type) return false;
-        
         this.modifiedTiles[key] = type;
         return true;
     }
-
     getModifiedTile(x, y) {
         return this.modifiedTiles[`${x},${y}`] || null;
     }
-
-    // --- MÉTODOS DE CRESCIMENTO ---
-    
     addGrowingPlant(x, y) {
         const key = `${x},${y}`;
-        if (!this.growingPlants[key]) {
-            this.growingPlants[key] = Date.now();
-        }
+        if (!this.growingPlants[key]) this.growingPlants[key] = Date.now();
     }
-
     removeGrowingPlant(x, y) {
         delete this.growingPlants[`${x},${y}`];
     }
-
-    // --- SINCRONIZAÇÃO DE ENTRADA ---
-
     getFullState() {
-        return {
-            tiles: this.modifiedTiles,
-            plants: this.growingPlants
-        };
+        return { tiles: this.modifiedTiles, plants: this.growingPlants };
     }
-
     applyFullState(stateData) {
         if (stateData) {
             this.modifiedTiles = stateData.tiles || {};

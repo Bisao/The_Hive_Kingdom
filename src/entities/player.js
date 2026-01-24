5ad8224138c852f9d92128a74efa9ac97c6d585b
Bisao <godofglobal@gmail.com>
2026-01-24 09:46:33 -0300

    Update player.js

--- src/entities/player.js
+++ src/entities/player.js
@@ -3,8 +3,6 @@ export class Player {
         this.id = id;
         this.nickname = nickname;
         this.isLocal = isLocal;
-        
-        // Posição
         this.pos = { x: 0, y: 0 };
         this.targetPos = { x: 0, y: 0 };
         this.speed = 0.15;
@@ -13,15 +11,11 @@ export class Player {
         // Status
         this.pollen = 0;
         this.maxPollen = 100;
-        
-        // NOVO: Sistema de Vida
         this.hp = 100;
         this.maxHp = 100;
 
-        // Sprites
         this.sprites = {};
-        const dirs = ['Up', 'Down', 'Left', 'Right', 'Idle', 'LeftIdle', 'RightIdle'];
-        dirs.forEach(d => {
+        ['Up', 'Down', 'Left', 'Right', 'Idle', 'LeftIdle', 'RightIdle'].forEach(d => {
             this.sprites[d] = new Image();
             this.sprites[d].src = `assets/Bee${d}.png`;
         });
@@ -42,10 +36,7 @@ export class Player {
                 else if(this.currentDir === 'Up' || this.currentDir === 'Down') this.currentDir = 'Idle';
             }
         } else {
-            // Interpolação Linear para movimento suave dos outros jogadores
-            // Se a distância for muito grande (respawn), teleporta direto
             const dist = Math.sqrt(Math.pow(this.targetPos.x - this.pos.x, 2) + Math.pow(this.targetPos.y - this.pos.y, 2));
-            
             if (dist > 5) {
                 this.pos.x = this.targetPos.x;
                 this.pos.y = this.targetPos.y;
@@ -56,12 +47,11 @@ export class Player {
         }
     }
 
-    // Método para renascer na colmeia
     respawn() {
         this.pos.x = 0;
         this.pos.y = 0;
         this.hp = this.maxHp;
-        this.pollen = 0; // Perde a carga
+        this.pollen = 0; 
         this.currentDir = 'Down';
     }
 
@@ -79,34 +69,9 @@ export class Player {
             ctx.beginPath(); ctx.arc(sX, sY, 10, 0, Math.PI*2); ctx.fill();
         }
 
-        // --- BARRA DE VIDA (Vermelha) ---
-        // Desenha sempre acima da barra de pólen
-        const barWidth = 32;
-        const barHeight = 4;
-        const hpY = sY - 45; // Posição Y da vida
-        
-        // Fundo Vermelho Escuro
-        ctx.fillStyle = "#550000"; 
-        ctx.fillRect(sX - barWidth/2, hpY, barWidth, barHeight);
-        
-        // Vida Atual (Vermelho Vivo)
-        ctx.fillStyle = "#e74c3c"; 
-        const hpPct = Math.max(0, this.hp / this.maxHp);
-        ctx.fillRect(sX - barWidth/2, hpY, barWidth * hpPct, barHeight);
+        // --- BARRAS REMOVIDAS (Agora estão no HUD) ---
 
-
-        // --- BARRA DE PÓLEN (Amarela) ---
-        if (this.pollen > 0) {
-            const pollenY = sY - 38; // Posição Y do pólen (abaixo da vida)
-            ctx.fillStyle = "#222"; 
-            ctx.fillRect(sX - barWidth/2, pollenY, barWidth, barHeight);
-            
-            ctx.fillStyle = "#f1c40f"; 
-            const polPct = this.pollen / this.maxPollen;
-            ctx.fillRect(sX - barWidth/2, pollenY, barWidth * polPct, barHeight);
-        }
-
-        // Nickname
+        // Apenas o Nickname
         ctx.fillStyle = "white"; 
         ctx.font = "bold 12px sans-serif"; 
         ctx.textAlign = "center";

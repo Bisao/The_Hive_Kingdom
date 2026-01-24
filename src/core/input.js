4fb17e58968344a74c98376cb4f223ef64b1bd91
Bisao <godofglobal@gmail.com>
2026-01-23 21:27:28 -0300

    Update input.js

--- src/core/input.js
+++ src/core/input.js
@@ -1,5 +1,23 @@
-export function initInput() {
-  window.addEventListener('keydown', e => {
-    console.log('Key pressed:', e.key);
-  });
+// src/core/input.js
+export class InputHandler {
+    constructor() {
+        this.keys = {};
+        window.addEventListener('keydown', e => this.keys[e.key.toLowerCase()] = true);
+        window.addEventListener('keyup', e => this.keys[e.key.toLowerCase()] = false);
+    }
+
+    getMovement() {
+        let x = 0, y = 0;
+        if (this.keys['w'] || this.keys['arrowup']) y -= 1;
+        if (this.keys['s'] || this.keys['arrowdown']) y += 1;
+        if (this.keys['a'] || this.keys['arrowleft']) x -= 1;
+        if (this.keys['d'] || this.keys['arrowright']) x += 1;
+        
+        // Normaliza para não correr mais rápido na diagonal
+        if (x !== 0 && y !== 0) {
+            x *= 0.707;
+            y *= 0.707;
+        }
+        return { x, y };
+    }
 }
